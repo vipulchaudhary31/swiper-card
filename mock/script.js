454a7dcaec4c8618ab35de7dbac14cc34f4a1975
vipulchaudhary31 <77533317+vipulchaudhary31@users.noreply.github.com>
2024-10-19 01:55:05 +0530

    Update script.js

--- mock/script.js
+++ mock/script.js
@@ -1,4 +1,5 @@
 var swiper = new Swiper('.swiper-container.two', {
+    // Current properties
     pagination: '.swiper-pagination',
     paginationClickable: true,
     effect: 'cards',
@@ -7,12 +8,32 @@ var swiper = new Swiper('.swiper-container.two', {
     slidesPerView: 'auto',
     cardsEffect: {
         slideShadows: false,
+        perSlideOffset: 18,     // Controls spacing between cards
+        perSlideRotate: 4,     // Adds rotation to non-active cards
+        rotate: true,          // Enable/disable rotation
+        modifier: 1            // Intensity of the effect
     },
-    coverflow: {
-        rotate: 0,
-        stretch: 100,
-        depth: 150,
-        modifier: 1.5,
-        slideShadows: false,
-    }
+    
+    // Additional properties you can add
+//    speed: 400,               // Transition speed in ms
+//    direction: 'horizontal',  // 'horizontal' or 'vertical'
+//    grabCursor: false,        // Changes cursor to grab when hovering
+//    initialSlide: 0,         // Starting slide index
+//    spaceBetween: 30,        // Space between slides in px
+    
+    // Navigation arrows
+    
+    
+//    // More pagination options
+//    pagination: {
+//        el: '.swiper-pagination',
+//        type: 'bullets',      // 'bullets', 'fraction', 'progressbar'
+//        clickable: true
+//    },
+//    
+//    // Autoplay settings
+//    autoplay: {
+//        delay: 2500,
+//        disableOnInteraction: false
+//    }
 });
